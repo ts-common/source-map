9d6745c75c4c97651dddf51bd12968681d38c790
Sergey Shandar <sergey.shandar@gmail.com>
2018-08-06 16:52:19 -0700

    0.0.0

--- index.ts
+++ index.ts
@@ -15,113 +15,22 @@ type Tracked<T extends Json|undefined> =
     T extends JsonPrimitive ? TrackedPrimitive<T> :
     undefined
 
-interface TrackedObject<T extends JsonObject> extends TrackedBase<T> {
-    readonly properties: { readonly [K in keyof T]: Tracked<T[K]> }
-}
-
-interface TrackedArray<T extends JsonArray> extends TrackedBase<T> {
-    readonly items: ReadonlyArray<Tracked<Json>>
-}
-
-/*
-import { StringMap, MutableStringMap } from "@ts-common/string-map"
-import { Json, JsonObject, JsonPrimitive } from "@ts-common/json"
-
-export const trackedObjectSymbol = Symbol("trackedObject")
-export const trackedPrimitiveSymbol = Symbol("trackedPrimitive")
-
-export interface SourceLink {
-    readonly fileUrl: string
-    readonly objectPath: ReadonlyArray<string|number>
-    readonly line: number
-    readonly column: number
-}
-
-export interface ObjectSourceLink {
-    readonly link: SourceLink
-    readonly primitiveProperties: StringMap<SourceLink>
-}
-
-export interface TrackedObjectBase {
-    readonly [trackedObjectSymbol]: ObjectSourceLink
-}
-
-export type TrackedObject<T extends object> = T & TrackedObjectBase
-
-export type TrackedArray<T> = ReadonlyArray<T> & TrackedObjectBase
-
-export interface TrackedPrimitive<T extends JsonPrimitive> {
-    readonly [trackedPrimitiveSymbol]: SourceLink
-    readonly value: T
-}
-
-export type TrackedBase = TrackedObjectBase|TrackedPrimitive<JsonPrimitive>
-
-export const isTrackedObject = (tracked: TrackedBase): tracked is TrackedObjectBase =>
-    (tracked as any)[trackedPrimitiveSymbol] === undefined
-
-export const getSourceLink = (tracked: TrackedBase): SourceLink =>
-    isTrackedObject(tracked) ? tracked[trackedObjectSymbol].link : tracked[trackedPrimitiveSymbol]
-
-export type Tracked<T extends Json|undefined> =
-    // null|string|...
-    T extends JsonPrimitive ? TrackedPrimitive<T> :
-    // JsonObject and JsonArray
-    T extends object ? TrackedObject<T> :
-    // undefined
-    undefined
-
-export type TrackedPropertySet<T extends JsonObject> = {
+type TrackedObjectProperties<T extends JsonObject> = {
     readonly [K in keyof T]: Tracked<T[K]>
 }
 
-interface MutableTrackedObject {
-    [trackedObjectSymbol]: ObjectSourceLink
+interface TrackedObject<T extends JsonObject> extends TrackedBase<T> {
+    readonly getProperties: () => TrackedObjectProperties<T>
 }
 
-export const addTrackedObject = <T extends object>(value: T, link: ObjectSourceLink): TrackedObject<T> => {
-    const result = value as (T & MutableTrackedObject)
-    result[trackedObjectSymbol] = link
-    return result
-}
+type Items<T> = T extends ReadonlyArray<infer U> ? U : never
 
-export const createArray = <T extends Json>(tracked: TrackedBase, it: Iterable<Tracked<T>>): TrackedArray<T> => {
-    const result: Array<T> = []
-    const primitiveProperties: MutableStringMap<SourceLink> = {}
-    for (const i of it) {
-        if (i === undefined) {
-            result.push(undefined as any)
-        }
-        if (isTrackedObject(i)) {
-            result.push(i)
-        }
-        // result.push(i)
-    }
-    return addTrackedObject(result, { link: getSourceLink(tracked), primitiveProperties:primitiveProperties })
-}
-*/
+type TrackedItems<T extends JsonArray> = Tracked<Items<T>>
 
-/*
-export const createArray = <I extends TrackedBase>(p: TrackedBase, it: Iterator<I>): TrackedArray<I> => {
-    const result: Array<I> = []
-    const sourceLink: ObjectSourceLink = {}
-    result[trackedObjectSymbol] = sourceLink
-}
-*/
+type TrackedArrayItems<T extends JsonArray> = {
+    readonly [K in keyof T & number]: Tracked<T[K]>
+} & ReadonlyArray<TrackedItems<T>>
 
-/*
-export interface SourceMap {
-    readonly createArray: <I extends TrackedBase>(
-        p: TrackedBase,
-        it: Iterator<I>
-    ) => TrackedArray<I>
-    readonly createStringMap: <I extends TrackedBase>(
-        p: TrackedBase,
-        it: Iterator<Entry<I>>,
-    ) => TrackedStringMap<I>
-    readonly createPropertySet: <T extends TrackedObject>(
-        p: TrackedBase,
-        factory: Factory<TrackedPropertySet<T>>
-    ) => T
+interface TrackedArray<T extends JsonArray> extends TrackedBase<T> {
+    readonly getItems: () => TrackedArrayItems<T>
 }
-*/
